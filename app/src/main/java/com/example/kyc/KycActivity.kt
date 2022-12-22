b4e5c86d03226f2cc8d92edcc944dfc72e9cfa31
prasenjitbanik <prasen267@gmail.com>
2022-12-22 17:35:07 +0530

    add validations

--- app/src/main/java/com/example/kyc/KycActivity.kt
+++ app/src/main/java/com/example/kyc/KycActivity.kt
@@ -1,11 +1,22 @@
 package com.example.test1
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.lifecycleScope
+import com.example.kyc.R
 import com.example.kyc.databinding.ActivityKycBinding
 import com.google.android.material.textfield.TextInputEditText
-
+import kotlinx.coroutines.flow.collectLatest
 
 
 class KycActivity : AppCompatActivity() {
@@ -22,13 +33,70 @@ class KycActivity : AppCompatActivity() {
 
         binding = ActivityKycBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setKycInfoText(binding)
+        with(binding) {
+            etPanNumber.doOnTextChanged { text, start, before, count ->
+                kycViewModel.panNumber.value = text.toString()
+            }
+            dayOfBirth.doOnTextChanged { text, start, before, count ->
+                kycViewModel.dayOfBirth.value = text.toString()
+            }
 
+            monthOfBirth.doOnTextChanged { text, start, before, count ->
+                kycViewModel.monthOfBirth.value = text.toString()
+            }
+            yearOfBirth.doOnTextChanged { text, start, before, count ->
+                kycViewModel.yearOfBirth.value = text.toString()
+            }
+        }
+
+        lifecycleScope.launchWhenCreated {
+            kycViewModel.panErrorMessage.observe(this@KycActivity) { _errorMessage ->
+                _errorMessage?.let {
+                    binding.tvErrorMessage.visibility = View.VISIBLE
+                    binding.etPanNumber.error = _errorMessage
+                }
+            }
+            kycViewModel.birthDateErrorMessage.observe(this@KycActivity) { _errorMessage ->
+                _errorMessage?.let {
+                    binding.tvErrorMessage.visibility = View.VISIBLE
+                    binding.tvErrorMessage.text = _errorMessage
+                }
+            }
+            kycViewModel.isFormValid.collectLatest { isValid ->
+                binding.btnNext.apply {
+                    isEnabled = isValid
+                    backgroundTintList = ColorStateList.valueOf(
+                        if (isValid) {
+                            ContextCompat.getColor(this@KycActivity, R.color.purple_700)
+                        } else {
+                            ContextCompat.getColor(this@KycActivity, R.color.gray)
+                        }
+                    )
+                }
+            }
+        }
 
         binding.btnNext.setOnClickListener {
+            Toast.makeText(this, "Details Submitted Succesfully", Toast.LENGTH_SHORT).show()
 
+        }
+        binding.btnNoPan.setOnClickListener {
+            finish()
         }
 
 
+    }
+
+    private fun setKycInfoText(binding: ActivityKycBinding) {
+        val spannable = SpannableString(resources.getString(R.string.kyc_info_label))
+        spannable.setSpan(
+            ForegroundColorSpan(resources.getColor(R.color.purple_700)),
+            106, // start
+            116, // end
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        binding.labelKycDisclaimer.text = spannable
     }
 
 
--- app/src/main/java/com/example/kyc/KycViewModel.kt
+++ app/src/main/java/com/example/kyc/KycViewModel.kt
@@ -1,8 +1,77 @@
 package com.example.test1
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
-class KycViewModel:ViewModel() {
+class KycViewModel : ViewModel() {
+
+    val panNumber = MutableStateFlow("")
+    val dayOfBirth = MutableStateFlow("")
+    val monthOfBirth = MutableStateFlow("")
+    val yearOfBirth = MutableStateFlow("")
+    val panPattern = Pattern.compile("[A-Z]{5}[0-9]{4}[A-Z]{1}")
+    var MAX_VALID_YR = 9999
+    var MIN_VALID_YR = 1800
+    val panErrorMessage: LiveData<String?>
+        get() = _panErrorMessage
+    private var _panErrorMessage = MutableLiveData<String?>()
+
+    val birthDateErrorMessage: LiveData<String?>
+        get() = _birthDateErrorMessage
+    private var _birthDateErrorMessage = MutableLiveData<String?>()
+
+    val isFormValid =
+        combine(panNumber, dayOfBirth, monthOfBirth, yearOfBirth) { _pan, _day, _month, _year -> val panMatcher: Matcher = panPattern.matcher(_pan)
+            val isPanvalid = panMatcher.matches()
+            val isValidBirthDate = try {
+                isValidBirthDate(_day.toInt(), month = _month.toInt(), year = _year.toInt())
+            } catch (e: NumberFormatException) {
+                false
+            }
+
+            if(isPanvalid.not()){
+                _panErrorMessage.postValue("Pan number is not valid")
+            }
+            if(isValidBirthDate.not()){
+                _birthDateErrorMessage.postValue("Please enter a correct Birthdate")
+            }
+            else{
+                _birthDateErrorMessage.postValue("")
+            }
+            isValidBirthDate and isPanvalid
+
+        }
+
+
+    fun isLeap(year: Int): Boolean {
+
+        return year % 4 == 0 &&
+                year % 100 != 0 ||
+                year % 400 == 0
+    }
+
+
+    fun isValidBirthDate(
+        day: Int,
+        month: Int,
+        year: Int
+    ): Boolean {
+        if (year > MAX_VALID_YR ||
+            year < MIN_VALID_YR
+        ) return false
+        if (month < 1 || month > 12) return false
+        if (day < 1 || day > 31) return false
+        if (month == 2) {
+            return if (isLeap(year)) day <= 29 else day <= 28
+        }
+        return if (month == 4 || month == 6 || month == 9 || month == 11
+        ) day <= 30 else true
+    }
 
 
 }
